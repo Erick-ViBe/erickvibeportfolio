2e1d9330441ed935ebafca0f4646bdcc728b9ad2
Erick-ViBe <erickvb12@gmail.com>
2021-03-15 23:18:00 -0600

    Responsive NavBar finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import {
+  ChakraProvider,
+  ColorModeScript,
+  theme,
+} from '@chakra-ui/react';
 
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
   document.getElementById('root')
 );
