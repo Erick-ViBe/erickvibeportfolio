5ae1dada62507a46b01827b1b965af0e75eb9062
Erick-ViBe <erickvb12@gmail.com>
2021-04-11 19:23:29 -0500

    Added bottom description

--- src/App/Components/Hero.js
+++ src/App/Components/Hero.js
@@ -9,38 +9,46 @@ import myphoto from '../../Assets/MyVectorST.png'
 
 export const Hero = () => {
   return (
-    <Flex
-      marginX='5%'
-      mt='90px'
-      flexWrap='wrap'
-      justifyContent='space-evenly'
-      alignItems='center'
-      id='Info'
-    >
-      <Image
-        src={myphoto}
-        minW='50%'
-        boxSize={['80%', '80%', '50%', '50%']}
-        transform= 'scaleX(-1)'
-        mb={{base: '25px', md:'none'}}
-      />
-      <Box
-        maxW={['80%', '80%', '45%', '45%']}
+    <>
+      <Flex
+        marginX='5%'
+        mt='90px'
+        flexWrap='wrap'
+        justifyContent='space-evenly'
+        alignItems='center'
+        id='Info'
       >
-        <Text
-          my='auto'
-          fontSize={['4xl', '5xl', '4xl', '6xl']}
-          align='center'
+        <Image
+          src={myphoto}
+          minW='50%'
+          boxSize={['80%', '80%', '50%', '50%']}
+          transform= 'scaleX(-1)'
+          mb={{base: '25px', md:'none'}}
+        />
+        <Box
+          maxW={['80%', '80%', '45%', '45%']}
         >
-          Hi, i'm
-          Erick ViBe
-          <br />
-          Im a Python
-          <br />
-          Backend Developer
-        </Text>
-      </Box>
-    </Flex>
+          <Text
+            my='auto'
+            fontSize={['4xl', '5xl', '4xl', '6xl']}
+            align='center'
+          >
+            Hi, i'm
+            Erick ViBe
+            <br />
+            Im a Python
+            <br />
+            Backend Developer
+          </Text>
+          <Text
+            fontSize={['sm', 'md', 'lg', 'xl']}
+            align='center'
+          >
+            Learning a little more every day, lover of technology and tacos, trying to learn frontend well
+          </Text>
+        </Box>
+      </Flex>
+    </>
       //Passionate about the evolution of technology
   )
 }
